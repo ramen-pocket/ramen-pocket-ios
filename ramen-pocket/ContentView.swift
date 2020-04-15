1330c417120551c22b3a0ac817557d6b29c642d1
yaoandy107 <yaoandy107@gmail.com>
2020-04-15 15:41:28 +0800

    Initial Commit

---
+++ ramen-pocket/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ramen-pocket
+//
+//  Created by 姚韋辰 on 2020/4/15.
+//  Copyright © 2020 姚韋辰. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
