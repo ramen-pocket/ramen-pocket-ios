5d0a030e4f7fde8cdee57e98173d7446e250097b
DevilTea <ch870814@gmail.com>
2020-05-28 00:59:12 +0800

    Add Error for general purpose

---
+++ ramen-pocket/Error.swift
@@ -0,0 +1,13 @@
+//
+//  Error.swift
+//  ramen-pocket
+//
+//  Created by 陳威任 on 2020/5/27.
+//  Copyright © 2020 姚韋辰. All rights reserved.
+//
+
+import Foundation
+
+enum Error: Swift.Error {
+    case general(reason: String)
+}
