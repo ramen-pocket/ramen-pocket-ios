6a7dd34fb92bdd7ba704c38d1277fdcebe2863e3
yaoandy107 <yaoandy107@gmail.com>
2020-06-08 06:41:41 +0800

    Change all parameter to Int? in Endpoint

--- ramen-pocket/Network/Endpoint.swift
+++ ramen-pocket/Network/Endpoint.swift
@@ -11,12 +11,12 @@ import Foundation
 public enum Endpoint {
     
     //MARK: - Store -
-    case stores, storeComments(storeId: String, recordId: String = "")
+    case stores, storeComments(storeId: Int, recordId: Int? = nil)
     
     //MARK: - User -
-    case userProfile(userId: String = "")
-    case userCollections(storeId: String = "", userId: String = "")
-    case userComments(userId: String = "")
+    case userProfile(userId: Int? = nil)
+    case userCollections(storeId: Int? = nil, userId: Int? = nil)
+    case userComments(userId: Int? = nil)
 }
 
 //MARK: - endpoint extension for url -
@@ -28,28 +28,28 @@ extension Endpoint {
         case .stores:
             return "stores"
         case .storeComments(storeId: let storeId, recordId: let recordId):
-            if (recordId == "") {
+            if (recordId == nil) {
                 return "stores/\(storeId)/comments"
             } else {
-                return "stores/\(storeId)/comments/\(recordId)"
+                return "stores/\(String(storeId))/comments/\(String(recordId!))"
             }
         case .userProfile(userId: let userId):
-            if (userId != "") {
-                return "user/\(userId)/profile"
+            if (userId != nil) {
+                return "user/\(String(userId!))/profile"
             } else {
                 return "user/profile"
             }
         case .userCollections(storeId: let storeId, userId: let userId):
-            if (storeId != "" && userId == "") {
+            if (storeId != nil && userId == nil) {
                 return "user/collections/:storeId"
-            } else if (storeId == "" && userId != "") {
+            } else if (storeId == nil && userId != nil) {
                 return "users/:id/collections"
             } else {
                 return "users/collections"
             }
         case .userComments(userId: let userId):
-            if (userId != "") {
-                return "user/\(userId)/comments"
+            if (userId != nil) {
+                return "user/\(String(userId!))/comments"
             } else {
                 return "user/comments"
             }
